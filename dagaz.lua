093083ff6f212216476dd393c84b61d3a275495b
asathoor <per@multimusen.dk>
2014-06-08 13:22:58 +0200

    count our days

--- dagaz.lua
+++ dagaz.lua
@@ -8,9 +8,26 @@ for n = 1, 366, 1 do
 	dagaz[n] = {nil,nil,nil,nil}
 end
 
--- DAYS
+------------------------------------------------------
+-- alternative dagaz array
+-- 
+
+local dagaz2 = {} -- the dates
+-- form month day e.g.
+
+dagaz2[12] = { [24] =  {"1224.png", "Juleaftens Dag"}}
+
+--test
+print("### Dagaz2: " .. dagaz2[12][24][2]) -- tekst juleaften
+
+------------------------------------------------------
+-- DATE, IMAGE, TEXT
 -- sample: dagaz[1] = number of the day - array: month, day, image, text
+
+-- Winter
 dagaz[1] = {1,1,"day_1.png","New Years Day"} -- test 
+
+-- Summer
 dagaz[157] = {6,6,"157.png","test"} -- test 
 
 -- Get values from the dagaz array
@@ -34,3 +51,33 @@ end
 -- test
 -- bDay(333,3) -- > nil
 -- bDay(157,3) -- > dagaz[num][ber]
+
+if (type(bDay(157,3)) == "string") 
+	then print("images/" .. tostring(bDay(2,3))) 
+end 
+
+------------------------------------------------------
+-- THE DAY NUMBER
+-- Enter month and date via the function dayNumber()
+
+maaned = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31} -- days of the year
+
+-- if leap year then m2 = 29 function here 
+-- ... code goes here ...
+
+-- Calculate the day number of some date
+local i = 1
+local day = 0
+local enDag = 0
+function dayNumber(month,enDag) 
+	while i < month do 
+		-- print(maaned[i])
+		day = day + maaned[i]
+		print(day + enDag)
+      	i = i + 1
+    end
+    return day + enDag
+end
+-- test
+dayNumber(12,27) 
+
